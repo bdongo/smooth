2f76cb9959bf0ca719caa6aa13d5bbad5517a5a5
brandon <brandon.w.leung@gmail.com>
2023-05-09 09:57:44 -0700

    comment out hello world?

--- backend/app.js
+++ backend/app.js
@@ -43,7 +43,7 @@ app.use(
     })
 );
 
-app.get("/", (req, res) => res.send("Hello World!!"));
+// app.get("/", (req, res) => res.send("Hello World!!"));
 
 // Attach Express routers
 const usersRouter = require('./routes/api/users');
