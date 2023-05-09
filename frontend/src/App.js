7f2ded189d6cab2e225f97472476bbe043626cb1
Christian <christianday954@ymail.com>
2023-05-08 17:03:56 -0700

    add demo login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import LoginForm from './components/SessionForms/LoginForm';
 import NavBar from './components/NavBar/NavBar';
 import SignupForm from './components/SessionForms/SignupForm';  
 import { useEffect, useState } from 'react';
+import { Route } from 'react-router-dom';
 
 
 function App() {
@@ -21,7 +22,7 @@ function App() {
     <>
     <NavBar />
     <Switch>
-      <AuthRoute exact path="/" component={SplashPage} />
+      <Route exact path="/" component={SplashPage} />
       <AuthRoute exact path="/login" component={LoginForm} />
       <AuthRoute exact path="/signup" component={SignupForm} />
     </Switch>
