942e1bac3ec2df32e8a3211d816084b51f9f997d
Christian <christianday954@ymail.com>
2023-05-09 17:37:04 -0700

    Edit splash

--- frontend/src/components/SplashPage/SplashPage.js
+++ frontend/src/components/SplashPage/SplashPage.js
@@ -1,18 +1,49 @@
 import './SplashPage.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
+import Scroll2 from './Scroll2';
+import Scroll3 from './Scroll3';
+import 'swiper/css';
+import 'swiper/css/autoplay'
+import { BsChevronDoubleDown } from 'react-icons/bs';
+import { FiChevronsDown } from 'react-icons/fi';
 
 function SplashPage() {
 
     return (
-        <div className="splash-page">
-            <div className='splash-1-text'>
-                Find your next 
-                <br/>
-                <p>experience</p>
+        <>
+        <div id='all-scrolls'>
+            <div className="splash-page">
+                <div className='splash-1-text-wrapper'> 
+                    <div className='splash-1-text'>
+                        Find your next 
+                    </div>
+                    <Swiper
+                        modules={[Autoplay]}
+                        spaceBetween={0}
+                        slidesPerView={1}
+                        loop={true}
+                        autoplay={{ 
+                            delay: 3300, 
+                            disableOnInteraction: false
+                        }}
+                        allowTouchMove={false}
+                    >
+                        <SwiperSlide>experience</SwiperSlide>
+                        <SwiperSlide>getaway</SwiperSlide>
+                        <SwiperSlide>destination</SwiperSlide>
+                        <SwiperSlide>vacation</SwiperSlide>
+                    </Swiper>
+                </div>
+                <div className='splash-1-image'>
+                    
+                </div>
+                <FiChevronsDown id='down-arrow'/>
             </div>
-            <div className='splash-1-image'>
-                
-            </div>
+            <Scroll2 />
+            <Scroll3 />
         </div>
+        </>
 
     )
 }
