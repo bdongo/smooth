8b5e44baf121712b892533bf936aa25b94d6f8f1
Christian <christianday954@ymail.com>
2023-05-10 16:43:26 -0700

    Add search

--- frontend/src/components/Search/SearchBar.js
+++ frontend/src/components/Search/SearchBar.js
@@ -4,35 +4,34 @@ import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchEvents } from "../../store/event";
 import { useParams } from 'react-router-dom'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 const Search = () => {
-  const queryInput = useParams();
   const [query, setQuery] = useState('')
   const [avgRating, setAvgRating] = useState('');
   const [avgPrice, setAvgPrice] = useState('');
   const [avgTime, setAvgTime] = useState('');
-  const [events, setEvents] = useState(null);
-  const [placeholder, setPlaceholder] = useState('Search by name or city') 
-  const searchRef = useRef(null);
-  const searchResultsRef = useRef(null);
+  // const [events, setEvents] = useState(null);
+  // const [placeholder, setPlaceholder] = useState('Search by name or city') 
+  // const searchRef = useRef(null);
+  // const searchResultsRef = useRef(null);
   const dispatch = useDispatch();
-
-  useEffect(()=> {
-    if (queryInput) {
-      setQuery(queryInput)
-    }
-  }, [queryInput])
+  const history = useHistory();
 
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // /search?query=${}?avgRating=${}
-    dispatch(fetchEvents(avgRating,avgPrice,avgTime, query))
+    const params = new URLSearchParams();
+    if (avgRating) params.append('rating', avgRating);
+    if (avgPrice) params.append('price', avgPrice);
+    if (avgTime) params.append('time', avgTime);
+    if (query) params.append('query', query);
+    window.location.href = `/search?${params}`
   }
 
     return (
       <>
-      <div clasName = "search-bar">
+      <div className = "search-bar">
           <form onSubmit={handleSubmit}>
           <label>
             Search:
