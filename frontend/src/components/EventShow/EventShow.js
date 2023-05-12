5e0f217b484a0b373f446e4cb8c12f671a0eda62
Vamshi Renduchintala <vrenduchintala17@gmail.com>
2023-05-11 18:44:17 -0700

    styling show page

--- frontend/src/components/EventShow/EventShow.js
+++ frontend/src/components/EventShow/EventShow.js
@@ -14,15 +14,14 @@ import { removeReview } from "../../store/reviews";
 const EventShow = () => {
     const dispatch = useDispatch();
     const {id} = useParams();
-    // obj id of union sq 645a7bffc64b62d6212c51b5 
     const event = useSelector(getEvent(id))
     const currentUser = useSelector((state) => state.session.user)
     const location = event?.location;
     const [map, setMap] = useState(null);
     const history = useHistory();
     const [deleteHelper, setDeleteHelper] = useState(false);
-    // console.log(location)
     const reviews = event?.reviews;
+    const [showCreateReview, setShowCreateReview] = useState(true);
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [])
@@ -31,7 +30,14 @@ const EventShow = () => {
          dispatch(fetchEvent(id))
     }, [deleteHelper])
 
-    
+    useEffect(() => {
+        event?.reviews?.forEach(review => {
+                if (review.author === currentUser._id ){
+                    setShowCreateReview(false);
+                }
+        })
+    }, [])
+   
 
     useEffect(() => {
         if (event) {
@@ -95,11 +101,26 @@ const EventShow = () => {
                 <p className="show-page-text about">{event?.description}</p>
             </div>
                 
-
+            <Link to={`/newReview?id=${id}`} id="link">
+                {setShowCreateReview ? <span className="make-review"> Create Review </span> : null }
+            </Link>
             <div className="review-container">
                 {event?.reviews?.map((review, idx) => (
+                    <> 
+                    { currentUser && (currentUser._id === review.author) && (
+                        <div className="buttons">
+                            <button className="remove-review" onClick={() => deleteReview(review._id)}>
+                                Remove Review
+                            </button>
+                            <Link id="update" to={`/updateReview?id=${review._id}&eventid=${id}`}>
+                                Update Review
+                            </Link>
+                        </div>
+                    )}
                     <div key={idx} className="review show-page-text">
-                        <p className="show-page-text">{review.text}</p>
+                        <p className="show-page-text">{review.text}
+    
+                        </p>
                         <div>
                         <div className="sub-header">
                             Rating: <RatingVisualizer score={review.rating} />
@@ -111,24 +132,14 @@ const EventShow = () => {
                             Time: <PieChart value={review.time} />
                         </div>
                         </div>
-                        {currentUser && (currentUser._id === review.author) && (
-                            <div>
-                                <button className="remove" onClick={() => deleteReview(review._id)}>
-                                    Remove Review
-                                </button>
-                                <Link to={`/updateReview?id=${review._id}&eventid=${id}`}>
-                                    Update Review
-                                </Link>
-                            </div>
-                        )}
+                
                         
                     </div>
+                    </>
                     
                 ))}
             </div>
-            <Link to={`/newReview?id=${id}`}>
-                Make a Review
-            </Link>
+            
         </div>
     )
 }
