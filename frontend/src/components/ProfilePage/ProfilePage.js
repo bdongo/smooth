fac944480997b501253177a40382d01eb0bf51c9
brandon <brandon.w.leung@gmail.com>
2023-05-17 15:13:54 -0700

    populate agenda index with events

--- frontend/src/components/ProfilePage/ProfilePage.js
+++ frontend/src/components/ProfilePage/ProfilePage.js
@@ -1,15 +1,29 @@
 import { useDispatch, useSelector } from "react-redux";
 import "./ProfilePage.css";
+import { getAgendas } from "../../store/agendas";
+import { useEffect } from "react";
+import { fetchAgendas } from "../../store/agendas";
 
 const ProfilePage = () => {
     const dispatch = useDispatch();
-    const currentUser = useSelector(state => state.session.currentUser)
+    const currentUser = useSelector(state => state.session.user)
+    const agendas = useSelector(getAgendas)
+
+    useEffect(() => {
+        dispatch(fetchAgendas(currentUser?._id))
+    }, [currentUser])
+    
 
     return (
-        <div>
+        <div className="profile-container">
             <h1>{currentUser?.username}</h1>
-            ProfilePage
+            <div className="itinerary-conatiner">
+                {agendas?.map(agenda => 
+                    <div>
 
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
